cb5c71bc2c4be15bcdd1b8b7d573dda189b857d9
Baris <barisbalcimusic@gmail.com>
2024-07-18 09:33:54 +0200

    last small update

--- backend/controllers/user/deleteUser.js
+++ backend/controllers/user/deleteUser.js
@@ -21,11 +21,11 @@ export const deleteUser = async (req, res, next) => {
     if (!deleteMessages || !deleteMessages.acknowledged)
       res.status(400).json({
         error: "DbError",
-        message: "User couldn't be deleted",
+        message: "Messages couldn't be deleted",
       });
 
     //RETURN SUCCESS MESSAGE
-    res.status(200).json({ message: "User successfuly deleted" });
+    res.status(200).json({ message: "User and messages successfuly deleted" });
   } catch (e) {
     next(e);
   }
