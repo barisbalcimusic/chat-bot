282bb9a44b8b5a1a0b45917e8720cd5682464291
Baris <barisbalcimusic@gmail.com>
2024-07-21 20:19:28 +0200

    some improvements for deploying

--- frontend/src/utils/deleteAccount.js
+++ frontend/src/utils/deleteAccount.js
@@ -1,12 +1,15 @@
 export const deleteAccount = async (userData) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/users/delete`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      //SEND USER ID TO FIND USER
-      body: JSON.stringify(userData),
-      credentials: "include",
-    });
+    const res = await fetch(
+      `https://backendfinalproject.onrender.com/api/users/delete`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        //SEND USER ID TO FIND USER
+        body: JSON.stringify(userData),
+        credentials: "include",
+      }
+    );
     const data = await res.json();
     return data;
   } catch (e) {
