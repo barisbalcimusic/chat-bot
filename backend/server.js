4d7f0e4696f6b372beeec4bce76d198a90c5e4be
Baris Balci <barisbalcimusic@gmail.com>
2024-09-09 22:04:48 +0200

    cors endpoint changed

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 //CORS ip settings
 app.use(
   cors({
-    origin: ["https://chatbotbybaris.onrender.com/", "http://localhost:5173"],
+    origin: ["https://chatbotbybaris.onrender.com", "http://localhost:5173"],
     credentials: true,
   })
 );
